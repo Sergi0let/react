c314346a124814d7c9a4ef86cac83281aefef2ec
Sergi0let <s.vashkevych@gmail.com>
2022-07-31 13:13:53 +0300

    init

--- lesson3-components-props/task1-components/src/Search.jsx
+++ lesson3-components-props/task1-components/src/Search.jsx
@@ -1,9 +1,12 @@
-export default (props) => (
-  <div className="search">
-    <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-    <div className="search__field">
-      <input type="text" className="search__input" />
-      <button className="search__button">Search</button>
+export default (props) => {
+  const elements = (
+    <div className="search">
+      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+      <div className="search__field">
+        <input type="text" className="search__input" />
+        <button className="search__button">Search</button>
+      </div>
     </div>
-  </div>
-);
+  );
+  return elements;
+};
