25630cf0e95d7b9f68d60f90816725a996d69073
Sergi0let <s.vashkevych@gmail.com>
2022-07-31 15:54:40 +0300

    init

--- lesson3-components-props/task3-composition/src/Avatar.jsx
+++ lesson3-components-props/task3-composition/src/Avatar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UserAvatar = (props) => {
-  return <img className="avatar" src={props.avatarUrl} alt={props.author} />;
+  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
 };
 
 export default UserAvatar;
--- lesson3-components-props/task3-composition/src/index.jsx
+++ lesson3-components-props/task3-composition/src/index.jsx
@@ -7,7 +7,7 @@ import './styles.scss';
 const rootElement = document.querySelector('#root');
 
 const userInfo = {
-  author: 'Tom',
+  name: 'Tom',
   avatarUrl: 'https://avatars1.githubusercontent.com',
 };
 
