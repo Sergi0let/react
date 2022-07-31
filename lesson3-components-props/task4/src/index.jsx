1d4a4f8438e93b182635623d8d4a9db9d7f96dc6
Sergi0let <s.vashkevych@gmail.com>
2022-07-31 17:12:36 +0300

    init

--- lesson3-components-props/task4/src/index.jsx
+++ lesson3-components-props/task4/src/index.jsx
@@ -7,9 +7,14 @@ import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-const formatDate = (date) => moment().format('YYYY') - date;
+const formatDate = (date) =>
+  moment(date).format('YYYY') - moment().format('YYYY');
 
 ReactDOM.render(
-  <Greeting firstName="Joe" lastName="Doe" birthDate={formatDate(2005)} />,
+  <Greeting
+    firstName="Joe"
+    lastName="Doe"
+    birthDate={formatDate(new Date('2001-01-01T11:11:11.819Z'))}
+  />,
   rootElement
 );
