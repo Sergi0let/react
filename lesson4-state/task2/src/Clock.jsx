c58cfd4298b9780642c61dc756625a364ca66e43
Sergi0let <s.vashkevych@gmail.com>
2022-08-02 08:57:44 +0300

    init

--- lesson4-state/task2/src/Clock.jsx
+++ lesson4-state/task2/src/Clock.jsx
@@ -6,42 +6,42 @@ import './clock.scss';
 const getTimeWithOffset = (offset) => {
   const currentTime = new Date();
   const utcOffset = currentTime.getTimezoneOffset() / 60;
-  const utcTime = new Date(
+  return new Date(
     currentTime.setHours(currentTime.getHours() + offset + utcOffset)
   );
-  return moment(utcTime).format('LTS');
 };
 
 class Clock extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      offset: props.offset + 1,
+      offset: props.offset,
+      date: moment(getTimeWithOffset(props.offset)).format('h:mm:ss A'),
     };
+  }
 
-    setInterval(() => {
-      this.setState({
-        time: getTimeWithOffset(this.state.offset),
-      });
-    }, 1000);
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: moment(getTimeWithOffset(this.state.offset)).format('h:mm:ss A'),
+    });
   }
 
   render() {
     return (
       <div className="clock">
         <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{this.state.time}</div>
+        <div className="clock__time">{this.state.date}</div>
       </div>
     );
   }
 }
 
 export default Clock;
-
-// hours: moment(getTimeWithOffset(this.state.offset)).format('h'),
-// minutes: moment(getTimeWithOffset(this.state.offset)).format('mm'),
-// seconds: moment(getTimeWithOffset(this.state.offset)).format('ss'),
-// day: moment(getTimeWithOffset(this.state.offset)).format('A'),
-// {this.state.hours}:{this.state.minutes}:{this.state.seconds}{' '}
-// {this.state.day}
