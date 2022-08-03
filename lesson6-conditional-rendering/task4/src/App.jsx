f78c2fd1f282bfab6794d4722c46a26fd713548d
Sergi0let <s.vashkevych@gmail.com>
2022-08-03 20:54:22 +0300

    init

--- lesson6-conditional-rendering/task4/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Status from './Status.jsx';
-
-const App = () => <Status isOnline={false} />;
-
-export default App;
--- lesson6-conditional-rendering/task4/src/index.jsx
+++ lesson6-conditional-rendering/task4/src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App.jsx';
-
 import './index.scss';
 
-const root = document.querySelector('#root');
+const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Status isOnline={false} />, rootElement);
