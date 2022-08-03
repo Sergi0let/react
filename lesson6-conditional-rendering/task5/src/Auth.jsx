fef541821e87cebe9671415f7986558f758ea048
Sergi0let <s.vashkevych@gmail.com>
2022-08-03 21:26:52 +0300

    init

---
+++ lesson6-conditional-rendering/task5/src/Auth.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Spinner from './Spinner.jsx';
+import Login from './Login.jsx';
+import Logout from './Logout.jsx';
+
+class Auth extends Component {
+  state = {
+    isLoggedIn: false,
+    showSpinner: false,
+  };
+
+  onLogin = () => {
+    this.setState({ showSpinner: true });
+
+    setTimeout(() => {
+      this.setState({
+        isLoggedIn: true,
+        showSpinner: false,
+      });
+    }, 2000);
+  };
+
+  onLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+
+  render() {
+    if (this.state.showSpinner) {
+      return <Spinner size={'30px'} />;
+    }
+
+    return this.state.isLoggedIn ? (
+      <Logout onLogout={this.onLogout} />
+    ) : (
+      <Login onLogin={this.onLogin} />
+    );
+  }
+}
+export default Auth;
