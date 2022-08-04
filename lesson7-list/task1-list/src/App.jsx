e5cc90ca61fb5450e9ef021512b35d7319ccde21
Sergi0let <s.vashkevych@gmail.com>
2022-08-04 13:11:39 +0300

    init

---
+++ lesson7-list/task1-list/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NumberList from './NumberList.jsx';
+
+const numbers = [1, 2, 3, 4, 5];
+
+const App = () => {
+  return <NumberList numbers={numbers} />;
+};
+
+export default App;
