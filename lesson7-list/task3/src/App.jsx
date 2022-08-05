bf0fd986c5e1906e21c28201bfd29511ce913a95
Sergi0let <s.vashkevych@gmail.com>
2022-08-05 15:23:31 +0300

    init

--- lesson7-list/task3/src/App.jsx
+++ lesson7-list/task3/src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+import TransactionsList from './TranitionList.jsx';
+
+const transactions = [
+  {
+    id: 'id-0',
+    from: 'USD',
+    to: 'EUR',
+    amount: 1200,
+    rate: 0.8,
+    time: '2019-01-10T17:08:35.447Z',
+  },
+  {
+    id: 'id-1',
+    from: 'USD',
+    to: 'UAH',
+    amount: 100000,
+    rate: 25.7,
+    time: '2019-01-10T18:22:35.447Z',
+  },
+  {
+    id: 'id-2',
+    from: 'EUR',
+    to: 'USD',
+    amount: 100,
+    rate: 1.1,
+    time: '2019-01-10T17:01:35.447Z',
+  },
+];
+
+const App = () => <TransactionsList transactions={transactions} />;
 
 export default App;
--- lesson7-list/task3/src/index.jsx
+++ lesson7-list/task3/src/index.jsx
@@ -2,19 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
+
 import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(<App />, rootElement);
-
-const Greeting = (props) => {
-  return (
-    <div className="greeting">
-      My name is {props.firstName} {props.lastName}. I'm {props.birthDate} years
-      old
-    </div>
-  );
-};
-
-export default Greeting;
