44905f6acb60be8cc861b2674246b34f5cdcbf56
Sergi0let <s.vashkevych@gmail.com>
2022-08-05 13:12:28 +0300

    init

--- test/src/index.jsx
+++ test/src/index.jsx
@@ -7,3 +7,14 @@ import App from './App.jsx';
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(<App />, rootElement);
+
+const Greeting = (props) => {
+  return (
+    <div className="greeting">
+      My name is {props.firstName} {props.lastName}. I'm {props.birthDate} years
+      old
+    </div>
+  );
+};
+
+export default Greeting;
