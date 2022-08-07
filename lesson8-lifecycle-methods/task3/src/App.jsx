d8c6c174f7f4eb4bc3893ea82167e1ba61938e8b
Sergi0let <s.vashkevych@gmail.com>
2022-08-07 12:04:21 +0300

    init

--- lesson8-lifecycle-methods/task3/src/App.jsx
+++ lesson8-lifecycle-methods/task3/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Dimensions from './Dimensions.jsx';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return <Dimensions />;
 };
 
 export default App;
--- lesson8-lifecycle-methods/task3/src/index.jsx
+++ lesson8-lifecycle-methods/task3/src/index.jsx
@@ -7,14 +7,3 @@ import App from './App.jsx';
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(<App />, rootElement);
-
-const Greeting = (props) => {
-  return (
-    <div className="greeting">
-      My name is {props.firstName} {props.lastName}. I'm {props.birthDate} years
-      old
-    </div>
-  );
-};
-
-export default Greeting;
