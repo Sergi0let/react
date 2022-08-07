4056610c9e7aa625adbe67edec9ecf690140e5c4
Sergi0let <s.vashkevych@gmail.com>
2022-08-07 17:17:13 +0300

    init

--- lesson9-form/task2-form-user/src/UserForm.jsx
+++ lesson9-form/task2-form-user/src/UserForm.jsx
@@ -24,7 +24,12 @@ class UserForm extends Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
+      <form
+        className="login-form"
+        onSubmit={() => {
+          this.props.onSubmit(this.state);
+        }}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
