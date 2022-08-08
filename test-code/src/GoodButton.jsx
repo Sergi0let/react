f86765f8ad3a25dfc9e5b3a2b6cc66bf3ecb3179
Sergi0let <s.vashkevych@gmail.com>
2022-08-08 16:02:27 +0300

    init

--- test-code/src/GoodButton.jsx
+++ test-code/src/GoodButton.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './fancyButton.scss';
 
-export default () => (
-  <button className="fancy-button" onClick={() => alert('Good job!')}>
-    Click me!
-  </button>
-);
+export default class GoodButton extends React.Component {
+  handleClick = (e) => alert(e.target.textContent);
+
+  render() {
+    return (
+      <button onClick={this.handleClick} className="fancy-button">
+        Click me!
+      </button>
+    );
+  }
+}
