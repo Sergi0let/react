ca56f7e711354d8b1c8569dd053d140abbb7bd07
Sergi0let <s.vashkevych@gmail.com>
2022-08-09 14:51:02 +0300

    init

--- test-code/src/App.jsx
+++ test-code/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import NumberList from './NumberList.jsx';
+import NumbersList from './NumbersList.jsx';
 
 import './index.scss';
 
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default () => <NumberList numbers={numArray} />;
+export default () => <NumbersList numbers={numArray} />;
