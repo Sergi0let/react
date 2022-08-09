38cbe4837bef645fe621c3fec2c68a4c4be1313e
Sergi0let <s.vashkevych@gmail.com>
2022-08-09 12:51:46 +0300

    refactor

--- lesson6-conditional-rendering/task2-i/src/index.jsx
+++ lesson6-conditional-rendering/task2-i/src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Mailbox from './Mailbox.jsx';
-import './index.scss';
 
-const rootElement = document.querySelector('#root');
+import App from './App.jsx';
 
-ReactDOM.render(<Mailbox unreadMessages={[1, 1]} />, rootElement);
+ReactDOM.render(<App />, document.querySelector('#root'));
