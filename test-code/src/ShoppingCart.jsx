6f973a83763813cc81e9a9f509c16891051f5b37
Sergi0let <s.vashkevych@gmail.com>
2022-08-10 21:30:07 +0300

    init

--- test-code/src/ShoppingCart.jsx
+++ test-code/src/ShoppingCart.jsx
@@ -4,7 +4,18 @@ import ProductsList from './ProductsList.jsx';
 
 class ShoppingCart extends React.Component {
   state = {
-    cartItems: [],
+    cartItems: [
+      {
+        id: '1',
+        name: 'iPhone 11',
+        price: 999,
+      },
+      {
+        id: '2',
+        name: 'iPad Pro',
+        price: 799,
+      },
+    ],
   };
   render() {
     const count = this.state.cartItems.length;
