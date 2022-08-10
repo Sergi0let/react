98d7a15e3f437581b3d083406c9e98974d5f47a1
Sergi0let <s.vashkevych@gmail.com>
2022-08-10 21:14:07 +0300

    init

--- test-code/src/CartTitle.jsx
+++ test-code/src/CartTitle.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-class CartTitle extends React.Component {
-  render() {
-    return (
-      <div className="cart-title">{`${this.props.userName}, you added ${this.props.count} items.`}</div>
-    );
+const CartTitle = ({ userName, count }) => {
+  let item;
+  if (count > 1) {
+    item = 'items';
+  } else {
+    item = 'item';
   }
-}
+  return (
+    <div className="cart-title">{`${userName}, you added ${count} ${item}.`}</div>
+  );
+};
 
 export default CartTitle;
-// const CartTitle = ({ userName, count }) => (
-//   <div className="cart-title">{`${userName}, you added ${count} items.`}</div>
-// );
