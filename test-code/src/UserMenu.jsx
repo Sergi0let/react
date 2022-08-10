a2bbf95ba351237579730ab94a2c4263fdf383ee
Sergi0let <s.vashkevych@gmail.com>
2022-08-10 17:24:50 +0300

    init

--- test-code/src/UserMenu.jsx
+++ test-code/src/UserMenu.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-export default ({ userData }) => {
-  if (!userData) {
-    return null;
+export default class UserMenu extends React.Component {
+  render() {
+    if (!this.props.userData) {
+      return null;
+    }
+    const { name, avatar_url } = this.props.userData;
+
+    return (
+      <div className="menu">
+        <span className="menu__greeting">Hello, {name}</span>
+        <span className="menu__name"> </span>
+        <img src={avatar_url} alt="User Avatar" className="menu__avatar" />
+      </div>
+    );
   }
-  const { name, avatar_url } = userData;
-  return (
-    <div className="menu">
-      <span className="menu__greeting">Hello, {name}</span>
-      <span className="menu__name"> </span>
-      <img src={avatar_url} alt="User Avatar" className="menu__avatar" />
-    </div>
-  );
-};
+}
--- test-code/src/UserProfile.jsx
+++ test-code/src/UserProfile.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-export default ({ userData }) => {
-  if (!userData) {
-    return null;
-  }
+export default class UserProfile extends React.Component {
+  render() {
+    if (!this.props.userData) {
+      return null;
+    }
 
-  const { name, location, avatar_url } = userData;
-  return (
-    <div className="user">
-      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">{name}</span>
-        <span className="user__location">{location}</span>
+    const { name, location, avatar_url } = this.props.userData;
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
