8378c0835e919aa4caeaf753801bee7a7e3cf472
Sergi0let <s.vashkevych@gmail.com>
2022-08-10 17:20:17 +0300

    init

--- test-code/src/UserProfile.jsx
+++ test-code/src/UserProfile.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-export default class UserProfile extends React.Component {
-  render() {
-    if (!this.props.userData) {
-      return null;
-    }
-
-    const { name, location, avatar_url } = this.props.userData;
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{name}</span>
-          <span className="user__location">{location}</span>
-        </div>
-      </div>
-    );
+export default ({ userData }) => {
+  if (!userData) {
+    return null;
   }
-}
+
+  const { name, location, avatar_url } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
