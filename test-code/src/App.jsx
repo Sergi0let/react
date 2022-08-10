954c1d43ea57d06382f495d213c7a99968c2133b
Sergi0let <s.vashkevych@gmail.com>
2022-08-10 21:03:18 +0300

    init

--- test-code/src/App.jsx
+++ test-code/src/App.jsx
@@ -26,7 +26,7 @@ class App extends React.Component {
     const { userData } = this.state;
     return (
       <div className="page">
-        <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+        <h1 className="title">{`Hello, John Doe`}</h1>
         <main className="content">
           <ShoppingCart userData={userData} />
           <Profile userData={userData} handleChange={this.handleChange} />
