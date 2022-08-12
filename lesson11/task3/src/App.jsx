e5f22ebd543cc8205b3e69c783754a98cd118dc1
Sergi0let <s.vashkevych@gmail.com>
2022-08-12 23:17:15 +0300

    init

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -15,6 +15,17 @@ class App extends React.Component {
   };
 
   render() {
+    let content;
+    if (this.state.isOpen) {
+      content = (
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and
+          other React features without writing a class.
+        </p>
+      );
+    } else {
+      null;
+    }
     return (
       <div className="app">
         <Expand
@@ -22,12 +33,7 @@ class App extends React.Component {
           title="Some title"
           togglerHandler={this.togglerHandler}
         >
-          {this.state.isOpen && (
-            <p>
-              Hooks are a new addition in React 16.8. They let you use state and
-              other React features without writing a class.
-            </p>
-          )}
+          {content}
         </Expand>
       </div>
     );
