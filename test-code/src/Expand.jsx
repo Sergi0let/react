db4b889fa13dd319e915b380ae170cbd09286982
Sergi0let <s.vashkevych@gmail.com>
2022-08-12 15:10:18 +0300

    штше

--- test-code/src/Expand.jsx
+++ test-code/src/Expand.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
 const Expand = ({ isClose, children, title, togglerHandler }) => {
   return (
     <div className="expand border">
@@ -10,9 +7,9 @@ const Expand = ({ isClose, children, title, togglerHandler }) => {
         <span className="expand__title">{title}</span>
         <button onClick={togglerHandler} className="expand__toggle-btn">
           {!isClose ? (
-            <FontAwesomeIcon icon={faChevronDown} size="2x" />
+            <i class="fas fa-chevron-down"></i>
           ) : (
-            <FontAwesomeIcon icon={faChevronUp} size="2x" />
+            <i class="fas fa-chevron-up"></i>
           )}
         </button>
       </div>
@@ -21,3 +18,15 @@ const Expand = ({ isClose, children, title, togglerHandler }) => {
   );
 };
 export default Expand;
+
+// підключення через Компоненту
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+// {
+//   !isClose ? (
+//     <FontAwesomeIcon icon={faChevronDown} size="2x" />
+//   ) : (
+//     <FontAwesomeIcon icon={faChevronUp} size="2x" />
+//   );
+// }
