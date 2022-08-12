39f644141b5e5087fdaa42077d72f4a6db432f0a
Sergi0let <s.vashkevych@gmail.com>
2022-08-12 12:27:57 +0300

    init

--- test-code/src/App.jsx
+++ test-code/src/App.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import Dialog from './Dialog.jsx';
+import Expand from './Expand.jsx';
 
 import './index.scss';
 
 export default class App extends React.Component {
   state = {
-    isOpen: false,
+    isClose: false,
   };
 
-  hideDialog = () => {
-    this.setState({ isOpen: false });
+  togglerHandler = () => {
+    console.log(this.state.isClose);
+    this.setState({ isClose: !this.state.isClose });
   };
-
-  showDialog = () => {
-    this.setState({ isOpen: true });
-  };
-
   render() {
     return (
       <div className="app">
-        <button onClick={this.showDialog} className="btn">
-          Show dialog
-        </button>
-        <Dialog
-          isOpen={this.state.isOpen}
-          onClose={this.hideDialog}
+        <Expand
+          isClose={this.state.isClose}
+          togglerHandler={this.togglerHandler}
           title="Some title"
         >
-          <p>Some text</p>
-        </Dialog>
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and
+            other React features without writing a class.
+          </p>
+        </Expand>
       </div>
     );
   }
---
+++ test-code/src/Expand.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Expand = ({ isClose, children, title, togglerHandler }) => {
+  let button;
+  if (!isClose) {
+    button = <FontAwesomeIcon icon={faChevronDown} />;
+  } else {
+    button = <FontAwesomeIcon icon={faChevronUp} />;
+  }
+
+  let content;
+  if (isClose) {
+    content = children;
+  } else {
+    content = '';
+  }
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button onClick={togglerHandler} className="expand__toggle-btn">
+          {button}
+        </button>
+      </div>
+      <div className="expand__content">{content}</div>
+    </div>
+  );
+};
+export default Expand;
