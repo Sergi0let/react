d923afd285405ba632046384ca7c44a853604ca2
Sergi0let <s.vashkevych@gmail.com>
2022-08-12 12:59:57 +0300

    init

--- test-code/src/App.jsx
+++ test-code/src/App.jsx
@@ -9,7 +9,6 @@ export default class App extends React.Component {
   };
 
   togglerHandler = () => {
-    console.log(this.state.isClose);
     this.setState({ isClose: !this.state.isClose });
   };
 
