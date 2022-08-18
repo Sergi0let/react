a9d5c0daa3b89aa90834ef5f4c785beb84c8a484
Sergi0let <s.vashkevych@gmail.com>
2022-08-18 12:39:40 +0300

    init

--- test-code/src/App.jsx
+++ test-code/src/App.jsx
@@ -1,32 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import User from './User.jsx';
-import './index.scss';
+import Dimensions from './Dimensions.jsx';
 
 const App = () => {
-  return (
-    <div className="page">
-      <Router>
-        <div className="page__content">
-          <h1>Users</h1>
-          <ul className="navigation">
-            <li className="navigation__item">
-              <Link to="/users/github">Github</Link>
-            </li>
-            <li className="navigation__item">
-              <Link to="/users/facebook">Facebook</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route path="/users/:userId" component={User} />
-            <Route path="/">
-              <span>Select a user please</span>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+  return <Dimensions />;
 };
 
 export default App;
