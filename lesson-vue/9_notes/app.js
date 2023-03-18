f291e745bb23df272e977f25c039d04c09e4c778
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-03-18 17:48:21 +0200

    save in Local Storage and get from Local Storage in Vue.js

--- lesson-vue/9_notes/app.js
+++ lesson-vue/9_notes/app.js
@@ -5,14 +5,46 @@ const App = {
       input: {
         value: '',
         placeholder: 'Type your note',
+        index: null,
       },
       notes: ['Note 1', 'Note 1', 'Note 3'],
     };
+  },
+  mounted() {
+    this.getNotes();
+  },
+  watch: {
+    notes: {
+      handler(updatedList) {
+        console.log(updatedList);
+        localStorage.setItem('notes', JSON.stringify(updatedList));
+      },
+      deep: true,
+    },
   },
   methods: {
     onSubmit() {
       this.notes.push(this.input.value);
       this.input.value = '';
+    },
+    remove(index) {
+      this.notes.splice(index, 1);
+    },
+    getNotes() {
+      const localNotes = localStorage.getItem('notes');
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes);
+      }
+    },
+
+    changeInputValue(index) {
+      this.input.index = index;
+    },
+
+    updateNote() {
+      this.notes[this.input.index] = this.input.value;
+      this.input.value = '';
+      this.input.index = null;
     },
   },
 };
